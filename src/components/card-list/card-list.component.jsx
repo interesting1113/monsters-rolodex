e70d09a4531ba87415f49b7e681714ed033b23b0
interesting1113 <sylviayang1113@gmail.com>
2022-11-22 11:03:12 +0900

    change to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,28 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 import Card from "./card/card.component";
 import "./card-list.style.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} key={monster.id}/>;
+    })}
+  </div>
+);
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <Card monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,19 +1,28 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 import "./search-box.style.css";
 
-class SearchBox extends Component {
-  render() {
-    const { className, onChangeHandler, placeholder } = this.props;
-    return (
-      <input
-        className={`search-box ${className}`}
-        type="search"
-        placeholder={placeholder}
-        onChange={onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBox = ({ className, onChangeHandler, placeholder }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+// class SearchBox extends Component {
+//   render() {
+//     const { className, onChangeHandler, placeholder } = this.props;
+//     return (
+//       <input
+//         className={`search-box ${className}`}
+//         type="search"
+//         placeholder={placeholder}
+//         onChange={onChangeHandler}
+//       />
+//     );
+//   }
+// }
 
 export default SearchBox;
